45a82be28c278cf2c17f4cf03724cac24fb12d37
Merwane Hamadi <merwanehamadi@gmail.com>
2023-04-12 16:12:51 -0700

    reduce size main script by moving user prompting methods into a separate python file
    move user_prompt methods in user_prompt files

--- scripts/main.py
+++ scripts/main.py
@@ -6,12 +6,12 @@ from memory import get_memory, get_supported_memory_backends
 import data
 import chat
 from colorama import Fore, Style
+from scripts.user_promt import construct_prompt
 from spinner import Spinner
 import time
 import speak
 from config import Config
 from json_parser import fix_and_parse_json
-from ai_config import AIConfig
 import traceback
 import yaml
 import argparse
@@ -180,87 +180,8 @@ def load_variables(config_file="config.yaml"):
     return full_prompt
 
 
-def construct_prompt():
-    """Construct the prompt for the AI to respond to"""
-    config = AIConfig.load()
-    if config.ai_name:
-        logger.typewriter_log(
-            f"Welcome back! ",
-            Fore.GREEN,
-            f"Would you like me to return to being {config.ai_name}?",
-            speak_text=True)
-        should_continue = utils.clean_input(f"""Continue with the last settings?
-Name:  {config.ai_name}
-Role:  {config.ai_role}
-Goals: {config.ai_goals}
-Continue (y/n): """)
-        if should_continue.lower() == "n":
-            config = AIConfig()
-
-    if not config.ai_name:
-        config = prompt_user()
-        config.save()
-
-    # Get rid of this global:
-    global ai_name
-    ai_name = config.ai_name
-
-    full_prompt = config.construct_full_prompt()
-    return full_prompt
 
 
-def prompt_user():
-    """Prompt the user for input"""
-    ai_name = ""
-    # Construct the prompt
-    logger.typewriter_log(
-        "Welcome to Auto-GPT! ",
-        Fore.GREEN,
-        "Enter the name of your AI and its role below. Entering nothing will load defaults.",
-        speak_text=True)
-
-    # Get AI Name from User
-    logger.typewriter_log(
-        "Name your AI: ",
-        Fore.GREEN,
-        "For example, 'Entrepreneur-GPT'")
-    ai_name = utils.clean_input("AI Name: ")
-    if ai_name == "":
-        ai_name = "Entrepreneur-GPT"
-
-    logger.typewriter_log(
-        f"{ai_name} here!",
-        Fore.LIGHTBLUE_EX,
-        "I am at your service.",
-        speak_text=True)
-
-    # Get AI Role from User
-    logger.typewriter_log(
-        "Describe your AI's role: ",
-        Fore.GREEN,
-        "For example, 'an AI designed to autonomously develop and run businesses with the sole goal of increasing your net worth.'")
-    ai_role = utils.clean_input(f"{ai_name} is: ")
-    if ai_role == "":
-        ai_role = "an AI designed to autonomously develop and run businesses with the sole goal of increasing your net worth."
-
-    # Enter up to 5 goals for the AI
-    logger.typewriter_log(
-        "Enter up to 5 goals for your AI: ",
-        Fore.GREEN,
-        "For example: \nIncrease net worth, Grow Twitter Account, Develop and manage multiple businesses autonomously'")
-    print("Enter nothing to load defaults, enter nothing when finished.", flush=True)
-    ai_goals = []
-    for i in range(5):
-        ai_goal = utils.clean_input(f"{Fore.LIGHTBLUE_EX}Goal{Style.RESET_ALL} {i+1}: ")
-        if ai_goal == "":
-            break
-        ai_goals.append(ai_goal)
-    if len(ai_goals) == 0:
-        ai_goals = ["Increase net worth", "Grow Twitter Account",
-                    "Develop and manage multiple businesses autonomously"]
-
-    config = AIConfig(ai_name, ai_role, ai_goals)
-    return config
 
 def parse_arguments():
     """Parses the arguments passed to the script"""
