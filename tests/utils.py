b8625434b95916cc67e3bb12b9487f3c07c4fe95
Merwane Hamadi <merwanehamadi@gmail.com>
2023-04-26 06:16:48 -0700

    instead of skipping the test, mark as xfail

--- tests/utils.py
+++ tests/utils.py
@@ -4,14 +4,18 @@ import os
 import pytest
 
 
+import pytest
+import functools
+import os
+
 def requires_api_key(env_var):
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             if not os.environ.get(env_var):
-                pytest.skip(
-                    f"Environment variable '{env_var}' is not set, skipping the test."
-                )
+                reason = f"Environment variable '{env_var}' is not set, marking the test as xfail."
+                xfail_marker = pytest.mark.xfail(reason=reason, strict=False)
+                return xfail_marker(func)(*args, **kwargs)
             else:
                 return func(*args, **kwargs)
 
