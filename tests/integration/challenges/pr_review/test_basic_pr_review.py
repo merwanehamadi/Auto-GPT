7dd1c78590e04de8d5befa0798d6a291f57e5bc8
Merwane Hamadi <merwanehamadi@gmail.com>
2023-05-19 08:58:55 -0700

    Useless comment

--- tests/integration/challenges/pr_review/test_basic_pr_review.py
+++ tests/integration/challenges/pr_review/test_basic_pr_review.py
@@ -1,8 +1,11 @@
 from types import SimpleNamespace
+
 import pytest
+
 from autogpt.workspace import Workspace
 from tests.integration.challenges.pr_review.base import run_tests
 from tests.utils import requires_api_key
+
 PR_LINK = "https://github.com/merwanehamadi/Auto-GPT/pull/116"
 PARAMETERS = SimpleNamespace(
     cycle_count=6,
@@ -14,8 +17,7 @@ PARAMETERS = SimpleNamespace(
     approved=True,
 )
 
+
 @requires_api_key("OPENAI_API_KEY")
-def test_basic_pr_review(
-    monkeypatch: pytest.MonkeyPatch, workspace: Workspace
-) -> None:
+def test_basic_pr_review(monkeypatch: pytest.MonkeyPatch, workspace: Workspace) -> None:
     run_tests(PARAMETERS, monkeypatch, workspace)
